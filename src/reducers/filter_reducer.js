f68ae2400c542b4c7361e35a747ea87b068db5ea
vasil-yordanov14 <vasil_sh@abv.bg>
2022-09-26 19:14:47 +0300

    filters text

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,7 +13,7 @@ const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
     let maxPrice = action.payload.map((p) => p.price);
     maxPrice = Math.max(...maxPrice);
-    console.log(maxPrice);
+
     return {
       ...state,
       all_products: [...action.payload],
@@ -50,6 +50,14 @@ const filter_reducer = (state, action) => {
       });
     }
     return { ...state, filtered_products: tempProducts };
+  }
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload;
+    return { ...state, filters: { ...state.filters, [name]: value } };
+  }
+  if (action.type === FILTER_PRODUCTS) {
+    console.log(`filtering products`);
+    return { ...state };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
