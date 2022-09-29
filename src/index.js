b33298ae6985ea3e3f67a362158f1e256a91aac3
vasil-yordanov14 <vasil_sh@abv.bg>
2022-09-29 20:19:10 +0300

    useContext setup

--- src/index.js
+++ src/index.js
@@ -8,14 +8,26 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// DOMAIN AUTH0 dev-35yycj1r.us.auth0.com
+// CLIENT ID KEg1Q46W8Igs8P2ijp6RzbngCxPI0cuW
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-35yycj1r.us.auth0.com"
+    clientId="KEg1Q46W8Igs8P2ijp6RzbngCxPI0cuW"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
 
   document.getElementById("root")
 );
