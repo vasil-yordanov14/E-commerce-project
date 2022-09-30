e49f19179e9d25afc0cca9dccd75a7213da1d451
vasil-yordanov14 <vasil_sh@abv.bg>
2022-09-30 18:22:19 +0300

    auth0 private route

--- src/App.js
+++ src/App.js
@@ -32,9 +32,9 @@ function App() {
           <Products />
         </Route>
         <Route exact path="/products/:id" children={<SingleProduct />} />
-        <Route exact path="/checkout">
+        <Private exact path="/checkout">
           <Checkout />
-        </Route>
+        </Private>
         <Route path="*">
           <Error />
         </Route>
